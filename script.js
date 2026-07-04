f24793b4c397cb9975aa568691b1921fcc3212cd
khozatreasure08-byte <khozatreasure08@gmail.com>
2026-07-04 21:55:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("SA Career Hub is running!");
+  const searchButton = document.querySelector("button");
+  const searchInput = document.querySelector("input");
+
+  searchButton.addEventListener("click", function () {
+    const search = searchInput.value.toLowerCase();
+
+    if (search.includes("shoprite")) {
+      alert("🛒 Shoprite Retail Assistant - Johannesburg");
+    } else if (search.includes("transnet")) {
+      alert("🚆 Transnet General Worker - Durban");
+    } else if (search.includes("administrator")) {
+      alert("🏥 Department of Health Administrator - Pretoria");
+    } else if (search === "") {
+      alert("Please type a job name.");
+    } else {
+      alert("No jobs found. More jobs coming soon!");
+    }
+  });
 });
